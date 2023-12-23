b012344971e84774e71c6c8ccde0759360c04109
alkathirikhalid <khalid@alkathirikhalid.com>
2023-12-23 10:17:41 +0800

    added initial voice skeletal structure

--- js/avoiceinmyhand.js
+++ js/avoiceinmyhand.js
@@ -1,3 +1,43 @@
-function init(){
-	document.getElementsByTagName("currentyear")[0].innerHTML = new Date().getFullYear().toString();
-}
+// This ensures that the DOM is fully loaded before trying to access elements
+document.addEventListener("DOMContentLoaded", function () {
+    // Update to current year
+    document.getElementsByTagName("currentyear")[0].innerHTML = new Date().getFullYear().toString();
+
+    // Access the <select> element
+    const selectElement = document.querySelector('select');
+    // Access the <textarea> element
+    const textareaElement = document.querySelector('textarea');
+    // Access the <button> element
+    const buttonElement = document.querySelector('button');
+    // Access Speech Synthesis - Holds text and voice
+    let utterance;
+
+    // Populate the <select> element with the voices
+    speechSynthesis.addEventListener('voiceschanged', function () {
+        // create utterance after voice has loaded
+        utterance = new SpeechSynthesisUtterance();
+        // Populate the select options here
+        for (let voice of speechSynthesis.getVoices()) {
+            let selected = voice.name === "Google US English" ? "selected" : "";
+            let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+            selectElement.insertAdjacentHTML("beforeend", option);
+        }
+    });
+
+    // Add a click event listener to the button
+    buttonElement.addEventListener('click', function () {
+        // This function will be called when the button is clicked
+        // Set text for the utterance
+        utterance.text = textareaElement.value;
+        // Set voice for the utterance (you can choose based on the selected option in your <select>)
+        utterance.voice = speechSynthesis.getVoices().find(voice => voice.name === selectElement.value);
+        // Only speak if there is text
+        if (textareaElement.value !== "") {
+
+        } else {
+            utterance.text = "Please enter text before clicking the button."
+            // Speak the utterance
+            speechSynthesis.speak(utterance);
+        }
+    });
+});
